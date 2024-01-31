6f36d75b43c0f2e9e486b159433fb34770f3ac27
Fran Sanchez <hola@beachlab.org>
2024-01-31 11:30:44 +0100

    housekeeping phase 2

--- translate-en.py
+++ translate-en.py
@@ -65,7 +65,7 @@ def wait_on_run(run, thread):
 changed_files = subprocess.check_output(['git', 'diff', '--name-only', '--cached']).decode().splitlines()
 for f in changed_files:
     if f.endswith('-es.md') and os.path.exists(f):
-        filename = f.replace('-es.md', '-en.md')
+        filename = os.path.join('..', '..', 'en', 'md', f.replace('-es.md', '-en.md'))
         print(f"Translating {f} to {filename}")
         with open(f, 'r') as file:
             text = file.read()
